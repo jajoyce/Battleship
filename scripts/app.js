f4e19efcadbb929754e3b9790acaf8baaab03ed3
jajoyce <jjoyce4@gmail.com>
2022-01-04 20:38:25 -0500

    Add ship class methods, hit, sink, and game win functionality. Refactor click handler, ship instances, variables. Refactor HTML and CSS.

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,17 @@
-const $enemyCells = $('.enemy-cell');
 const $playerCells = $('.player-cell');
+const $enemyCells = $('.enemy-cell');
 const $message1 = $('#message-1');
 const $message2 = $('#message-2');
+const $message3 = $('#message-3');
 const $restart = $('#restart');
 
-let enemyCells = $enemyCells;
+let gameOver = false;
+let sides = [
+    {name: "Player", shipsLeft: 5}, 
+    {name: "Enemy", shipsLeft: 5}
+];
 let playerCells = $playerCells;
+let enemyCells = $enemyCells;
 
 function addXYCoordinates(cells) {
     for (let i = 0; i < cells.length; i++) {
@@ -52,61 +58,108 @@ $enemyCells.on('click', enemyCellClick);
 $playerCells.on('click', enemyCellClick)
 
 function enemyCellClick() {
-    $message1.text(`Firing at ${this.y}${this.x}...`);
-    $message1.css({fontSize: '26px'});
-    if (this.occupied) {
-        $(this).text('X');
-        $(this).css({color: '#b44', backgroundColor: '#222'});
-        $message2.text('HIT!');
-        $message2.css({fontSize: '60px', color: '#b44'});
-        console.log('hit');
-    } else {
-        $(this).text('/');
-        $(this).css({backgroundColor: '#aaf'});
-        $message2.text('MISS');
-        $message2.css({fontSize: '60px', color: '#88d'});
-        console.log('miss');
+    if (!gameOver) {
+        if (this.shotAt) {
+            $message1.removeAttr('style');
+            $message1.text("Select a an open square!");
+            $message2.removeAttr('style');
+            $message2.text("You can't fire at the same square twice.");
+        } else {
+            this.shotAt = true;
+            $message1.text(`Firing at ${this.y}${this.x}...`);
+            $message1.css({fontSize: '26px'});
+            if (this.occupied) {
+                $(this).text('X');
+                $(this).css({color: '#b44', backgroundColor: '#222'});
+                $message2.text('HIT!');
+                $message2.css({fontSize: '60px', color: '#b44'});
+                this.shipHere.takeHit();
+                console.log(`hit ${this.shipHere.name}`);
+            } else {
+                $(this).text('/');
+                $(this).css({backgroundColor: '#aaf'});
+                $message2.text('MISS');
+                $message2.css({fontSize: '60px', color: '#88d'});
+                console.log('miss');
+            }
+        }
+        console.log(`Cell coordinates: ${this.y} ${this.x}`);
     }
-    console.log(`Cell coordinates: ${this.y} ${this.x}`)
 }
 
 class Ship {
-    constructor(cellLength, direction, cellsOccupied) {
+    constructor(side, name, cellLength, direction, cellsOccupied) {
+        this.side = side;
+        this.name = name;
         this.cellLength = cellLength;
         this.direction = direction;
         this.cellsOccupied = cellsOccupied;
+        this.hitsTaken = 0;
+        this.sunk = false;
     }
-    placeOnPlayerBoard() {
-        for (let cell of this.cellsOccupied) {
-            playerCells[cell].occupied = true;
-            playerCells[cell].style.backgroundColor = '#333';
+    placeOnBoard() {
+        if (this.side) {
+            for (let cell of this.cellsOccupied) {
+                enemyCells[cell].occupied = true;
+                enemyCells[cell].shipHere = this;
+            }
+        } else {
+            for (let cell of this.cellsOccupied) {
+                playerCells[cell].occupied = true;
+                playerCells[cell].shipHere = this;
+                playerCells[cell].style.backgroundColor = '#333';
+            }
         }
     }
-    placeOnEnemyBoard() {
-        for (let cell of this.cellsOccupied) {
-            enemyCells[cell].occupied = true;
+    takeHit() {
+        this.hitsTaken++;
+        this.checkSunk();
+        console.log(`${this.name} takes hit, has taken ${this.hitsTaken} hits total.`);
+    }
+    checkSunk() {
+        if (this.hitsTaken === this.cellLength) {
+            this.sunk = true;
+            sides[this.side].shipsLeft--;
+            $message3.css({fontSize: '26px'});
+            $message3.text(`You sank the enemy's ${this.name}!`);
+            console.log(`SANK ${this.name}`);
+            if (!sides[this.side].shipsLeft) {
+                gameOver = true;
+                $message1.css({fontSize: "30px"});
+                $message1.text('Game Over, YOU WIN!');
+                console.log('GAME OVER, YOU WIN!');
+            }
         }
     }
 }
 
 // Placeholder simple static ships for now
 
-let carrier = new Ship (5, 'vertical', [28, 38, 48, 58, 68]);
-let battleship = new Ship (4, 'horizontal', [81, 82, 83, 84]);
-let destroyer = new Ship (3, 'vertical', [32, 42, 52]);
-let submarine = new Ship (3, 'horizontal', [13, 14, 15]);
-let patrol = new Ship (2, 'horizontal', [55, 56]);
+let playerCarrier = new Ship (0, 'Aircraft Carrier', 5, 'vertical', [28, 38, 48, 58, 68]);
+let playerBattleship = new Ship (0, 'Battleship', 4, 'horizontal', [81, 82, 83, 84]);
+let playerDestroyer = new Ship (0, 'Destroyer', 3, 'vertical', [32, 42, 52]);
+let playerSubmarine = new Ship (0, 'Submarine', 3, 'horizontal', [13, 14, 15]);
+let playerPatrol = new Ship (0, 'Patrol Boat', 2, 'horizontal', [55, 56]);
+
+let enemyCarrier = new Ship (1, 'Aircraft Carrier', 5, 'vertical', [28, 38, 48, 58, 68]);
+let enemyBattleship = new Ship (1, 'Battleship', 4, 'horizontal', [81, 82, 83, 84]);
+let enemyDestroyer = new Ship (1, 'Destroyer', 3, 'vertical', [32, 42, 52]);
+let enemySubmarine = new Ship (1, 'Submarine', 3, 'horizontal', [13, 14, 15]);
+let enemyPatrol = new Ship (1, 'Patrol Boat', 2, 'horizontal', [55, 56]);
+
+// console.log(carrier);
 
 
-carrier.placeOnPlayerBoard();
-carrier.placeOnEnemyBoard();
-battleship.placeOnPlayerBoard();
-battleship.placeOnEnemyBoard();
-destroyer.placeOnPlayerBoard();
-destroyer.placeOnEnemyBoard();
-submarine.placeOnPlayerBoard();
-submarine.placeOnEnemyBoard();
-patrol.placeOnPlayerBoard();
-patrol.placeOnEnemyBoard();
+playerCarrier.placeOnBoard();
+enemyCarrier.placeOnBoard();
+playerBattleship.placeOnBoard();
+enemyBattleship.placeOnBoard();
+playerDestroyer.placeOnBoard();
+enemyDestroyer.placeOnBoard();
+playerSubmarine.placeOnBoard();
+enemySubmarine.placeOnBoard();
+playerPatrol.placeOnBoard();
+enemyPatrol.placeOnBoard();
+
 
 
